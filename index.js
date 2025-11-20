bafb49ad7a245323456a885985fde0f8e92d89f7
Elizabeth salazar <liza4231@gmail.com>
2025-11-19 19:36:29 -0600

    1st attempt

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+        users: []
+    };
 
+    for (let arr of args) {
+      combinedObject.users = [...combinedObject.users, ...arr];
+    }
+
+    const today = new Date().toString("M/d/yyyy");
+    combinedObject.merge_date = today;
+
+    return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
